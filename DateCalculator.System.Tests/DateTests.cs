2f0b4c04f9f9dabcde9c0494fb80e0f8b6f1b806
InaDelgado <inaianadelgado7@gmail.com>
2021-10-27 14:29:38 -0300

    Included new project about unit tests with xUnit

---
+++ DateCalculator.System.Tests/DateTests.cs
@@ -0,0 +1,15 @@
+using System;
+using DateCalculator.System;
+using Xunit;
+
+namespace DateCalculator.System.Tests
+{
+    public class DateTests
+    {
+        [Fact]
+        public void Should_be_return_error_when_operation_is_not_decrease_or_increase()
+        {
+
+        }
+    }
+}
